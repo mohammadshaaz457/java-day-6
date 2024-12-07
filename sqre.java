59eec987feac235de9acb84bda7679eead6ec0f3
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-07 10:54:43 +0530

    Add files via upload

---
+++ sqre.java
@@ -0,0 +1,34 @@
+public class sqre 
+{
+    public static void main(String args[])
+    {
+        
+        int n=4;
+
+       
+        for (int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j==0 || i==0 || i==n-1 || j==n-1)
+                System.out.print("*"+" ");
+
+            else
+            
+            
+            System.out.print(" "+" ");
+
+            }
+            
+        System.out.println();
+            
+        
+        
+    }
+    
+}
+}
+
+
+    
+
